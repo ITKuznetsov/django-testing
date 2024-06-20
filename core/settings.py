bf07b23f7ec8a667008d01aa30e85f6e3f7c5f0d
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-06-20 09:57:38 +0300

    starting drf tutorial

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'index',
+    'snippets',
 
     'debug_toolbar',
     'rest_framework',
--- index/urls.py
+++ index/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+appname = 'index'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
