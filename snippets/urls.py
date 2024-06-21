d734b60102fb9735368e3822820358e9dc321c7e
Nikita Kuznetsov <cskuznetsov@gmail.com>
2024-06-21 15:26:40 +0300

    complete DRF tutorial 2

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 appname = 'snippets'
@@ -8,3 +10,5 @@ urlpatterns = [
     path('', views.snippet_list),
     path('<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
